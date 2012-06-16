b12c9d6db6b7919b263c91d278168eb1a3ccdfe9
antmyth <antonio.p.nascimento@gmail.com>
2012-06-16 02:11:40 +0100

    setup and created Marker as example of companion objects

---
+++ messinWithScala/src/main/scala/Marker.scala
@@ -0,0 +1,19 @@
+class Marker(val color: String) {
+    println("Creating " + this)
+
+    override def toString = "Marker color:" + color
+}
+
+object Marker {
+    private val markers = Map(
+        "red" -> new Marker("Red"),
+        "green" -> new Marker("Green"),
+        "blue" -> new Marker("Blue")
+    )
+
+    def primaryColors="red, green, blue"
+
+    def getMarker(color: String) = {
+        if (markers.contains(color)) markers(color) else null
+    }
+}
---
+++ messinWithScala/src/main/scala/UseMarker.scala
@@ -0,0 +1,9 @@
+object UseMarker {
+    def main(args:Array[String]){
+        println(Marker getMarker "blue")
+        println(Marker getMarker "blue")
+        println(Marker getMarker "red")
+        println(Marker getMarker "red")
+        println(Marker primaryColors)
+    }
+}
